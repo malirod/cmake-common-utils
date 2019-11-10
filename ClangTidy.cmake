50fcd33bd56a73544b19c7dadc97da6f03a9f796
Rodion Malinovsky <rodionmalino@gmail.com>
2019-11-10 14:28:12 +0200

    Automate search of run-clang-tidy

--- ClangTidy.cmake
+++ ClangTidy.cmake
@@ -4,12 +4,16 @@
 message(STATUS "Setup target: clang-tidy")
 if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
     find_program(CLANG_TIDY_EXE NAMES "clang-tidy")
-    find_program(RUN_CLANG_TIDY_EXE NAMES "run-clang-tidy.py")
 
     if (NOT CLANG_TIDY_EXE)
         message(WARNING "clang-tidy not found!")
     endif()
 
+    find_package(LLVM REQUIRED CONFIG)
+    message(STATUS "Found LLVM ${LLVM_PACKAGE_VERSION}")
+    message(STATUS "LLVM prefix: ${LLVM_INSTALL_PREFIX}")
+
+    find_program(RUN_CLANG_TIDY_EXE NAMES "run-clang-tidy.py" PATHS "${LLVM_INSTALL_PREFIX}/share/clang")
     if (NOT RUN_CLANG_TIDY_EXE)
         message(WARNING "run-clang-tidy.py not found!")
     endif()
